bf248f361f184f141940c408d47a18c817ad26d0
naomi cui <naomi@nowme.ca>
2020-09-01 12:51:20 -0400

    latest build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f7317997e718993ef64fa998d253cca.js"
+  "/precache-manifest.fbbc8cb89dd178f7b4bab14d36fb91b4.js"
 );
 
 self.addEventListener('message', (event) => {
