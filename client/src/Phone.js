3e29163a14bf173450b162b98cf2abecba0dbabf
naomi cui <naomi@nowme.ca>
2020-09-03 17:17:24 -0400

    change Phone to PureComponent #44

--- client/src/Phone.js
+++ client/src/Phone.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Button from './Button';
 import Parallax from 'parallax-js';
 
-export default class Phone extends Component {
+export default class Phone extends PureComponent {
   constructor(props) { 
     super(props);
     this.classes = "bg-layer";
