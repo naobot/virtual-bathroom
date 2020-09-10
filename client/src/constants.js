e697c57b8be2643cd4581e861ced182481592465
naomi cui <naomi@nowme.ca>
2020-09-10 13:01:08 -0400

    new text conversations

--- client/src/constants.js
+++ client/src/constants.js
@@ -43,88 +43,73 @@ export const CONVOS = [
     {
       time: '11:34 AM',
       from: 'Mom',
-      message: 'I need you to help me write a complaint letter',
+      message: 'I need you to help me write a complaint letter for work',
     },
     {
       time: '11:34 AM',
-      from: 'Me',
-      message: 'OK... What is it about? Is it for work?',
-    },
-    {
-      time: '1:11 PM',
       from: 'Mom',
-      message: 'I’ll call you tonight to tell you. What time?',
-    },
-  ],
-  [
-    {
-      time: '12:58 PM',
-      from: 'Me',
-      message: 'hey sorry I don’t think I’ll be able to meet up tonite... Just got hit with wave of tiredness and need to lie down... I’ll text u sometime tomorrow tho?',
+      message: 'I will call you tonight',
     },
     {
-      time: '1:24 PM',
-      from: 'Micah',
-      message: 'ya of course',
+      time: '11:35 AM',
+      from: 'Mom',
+      message: 'And what is the password for the tablet?',
     },
     {
-      time: '1:24 PM',
-      from: 'Micah',
-      message: 'r u ok?',
-    },
-    {
-      time: '1:24 PM',
-      from: 'Micah',
-      message: 'do u need anything?',
+      time: '11:35 AM',
+      from: 'Mom',
+      message: 'Love, Mom',
     },
   ],
   [
     {
       time: '3 days ago',
       from: 'Me',
-      message: 'Can you please send someone to take a look at our fridge? The freezer doesn’t seem to be working properly. It’s not cold enough even though we have it on the coldest setting, and we’ve had to throw out the food we had in there.<br/>Thanks. LMK.',
+      message: 'Can you please send someone to take a look at our fridge? The freezer isn’t working, we have it on the coldest setting but it’s not cold. We’ve had to throw out the food we had in there. Thanks.',
     },
     {
       time: '1 day ago',
       from: 'Me',
-      message: 'Hello, I’m writing again re. our fridge. Can you please let me know when you are going to send someone to look at it?',
+      message: 'Hello, I’m writing again re. our fridge. It’s broken and the settings don’t work. Can you please let me know when you are going to send someone to look at it?',
     },
     {
       time: '1:58 PM',
       from: 'Landlord',
-      message: 'The fridge is new so there shouldn’t be any problem with it. Have you tried changing the setting to the coldest temperature?',
+      message: 'The fridge is new so there shouldn’t be any problem with it. Have you tried changing it to the coldest setting?',
     },
   ],
   [
     {
-      time: '1:24 PM',
-      from: 'Lily',
-      message: "I'm so fucking annoyed",
+      time: '10:24 AM',
+      from: 'Me', 
+      message: "I had a really nice time with u !"
     },
     {
-      time: '2:01 PM',
+      time: '10:45 AM',
+      from: 'Sky',
+      message: 'yes me too!!'
+    },
+    {
+      time: '11:20 AM',
+      from: 'Sky',
+      message: 'When can I kiss u again?!'
+    },
+  ],
+  [
+    {
+      time: '11:30 PM',
+      from: 'Lily',
+      message: "hey is 6pm still good to work on the thing tonight?",
+    },
+    {
+      time: '12:01 PM',
       from: 'Me',
-      message: "oh no what happened?",
+      message: "hey sorry I don’t think I can do tonite anymore… I had a really long day, I want to go home and lie down. Can we do tm?",
     },
     {
-      time: '2:01 PM',
+      time: '12:40 PM',
       from: 'Lily',
-      message: "Vincent is so fucking selfish and irresponsible",
-    },
-    {
-      time: '2:01 PM',
-      from: 'Lily',
-      message: "I was supposed to go see my parents for the first time in a month but now I have to cancel",
-    },
-    {
-      time: '2:02 PM',
-      from: 'Lily',
-      message: "Because maybe vincent exposed us to covid",
-    },
-    {
-      time: '2:03 PM',
-      from: 'Lily',
-      message: "Anyway I yelled at him but can you also talk to him for me?????? I feel like he doesn’t listen to me",
+      message: "oh no im sorry to hear! I’m busy tomorrow and the rest of this week tho, tonight is the only time I have free :(",
     },
   ],
 ];
