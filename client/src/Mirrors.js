1e520d207e11810a2918f7da36f09c03498d3e5e
naomi cui <naomi@nowme.ca>
2020-09-14 20:17:35 -0400

    add //freeourpee to routing

--- client/src/Mirrors.js
+++ client/src/Mirrors.js
@@ -49,28 +49,26 @@ class Mirrors extends PureComponent {
                       <dt>Graffiti:</dt>
                       <dd><a href="www.haeahnkwon.com" target="_blank">Haeahn Kwon</a></dd>
                       <dd><a href="https://caleysweetnsour.productions" target="_blank">Caley Feeney</a></dd>
-                      <dt>Audio descriptive guide:</dt>
-                      <dd>Aliya Pabani</dd>    
-                    </dl>
-                    <dl>
+                      <dt>Audio description:</dt>
+                      <dd><a href="https://twitter.com/aliyapabani" target="_blank">Aliya Pabani</a></dd>    
                       <dt>Curator:</dt>
                       <dd>Jenn Jackson</dd>
-                      <div>With contributions from <strong>Jon McCurley</strong></div>
                     </dl>
                     <p>
-                      We acknowledge the support of the <strong>Canada Council for the Arts</strong><br/>
-                      <img className="cca-logo" src={CCALogo} alt="Canada Council for the Arts" />
+                      With contributions from <strong>Jon McCurley</strong>
                     </p>
                     <p>
+                      We acknowledge the support of the <strong>Canada Council for the Arts</strong>.<br/>
                       <em>Make-Believe Bathroom</em> was produced with the support of <a href="https://www.sfu.ca/galleries.html" target="_blank"><strong>SFU Galleries</strong></a>.<br/>
+                      <img className="cca-logo" src={CCALogo} alt="Canada Council for the Arts" />
                       <img src="https://virtual-bathroom-assets.s3.us-east-2.amazonaws.com/SFUGalleries_logo.png" />
                     </p>
                   </div>
                   <div className="col-2">
                     <h2><a href="http://freeourpee.publicbathroom.online/">TOILETS FOR ALL</a></h2>
-                    <a href="http://freeourpee.publicbathroom.online/"><img src="https://virtual-bathroom-assets.s3.us-east-2.amazonaws.com/freeourpee/washfinals-small.png" /></a>
+                    <a href="http://freeourpee.publicbathroom.online/"><img id="free-our-pee-logo" src="https://virtual-bathroom-assets.s3.us-east-2.amazonaws.com/freeourpee/washfinals-small.png" /></a>
                     <p>A sticker project in collaboration with TO Toilet Codes. Read more and download the stickers <a href="http://freeourpee.publicbathroom.online/">here</a>.</p>
-                    <h2>SUBMIT A POSTER</h2>
+                    <h2><a href="mailto:amyclam@gmail.com">SUBMIT A POSTER</a></h2>
                     <p>To post a poster or public notice to the <em>Make-Believe Bathroom</em>, please send a PNG file to <strong>amyclam@gmail.com</strong>.</p>
                   </div>
                 </div>
