a4039178115bd3e7312402f6ac091b2770db517b
naobot <naomi@nowme.ca>
2025-02-14 21:20:48 +0900

    ES6

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const { urlencoded, json } = pkg;
 import cors from 'cors';
 import Pusher from 'pusher';
 import Datastore from 'nedb';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: '.env' });
 
